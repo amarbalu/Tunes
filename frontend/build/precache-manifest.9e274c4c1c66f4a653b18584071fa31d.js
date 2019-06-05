2eb2679c83ec8f1e26489cdc1f63067af1d7eadb
Your Name <inboxamarbalu@gmail.com>
2019-06-05 17:42:04 +0530

    .git ignore update

---
+++ frontend/build/precache-manifest.9e274c4c1c66f4a653b18584071fa31d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7a9caf702a55801c6b3c57bde3e942",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bc7b095ca83aff4dcf4",
+    "url": "/static/css/2.f881d4de.chunk.css"
+  },
+  {
+    "revision": "e15371fab3902af6e40d",
+    "url": "/static/css/main.7a49d99a.chunk.css"
+  },
+  {
+    "revision": "9bc7b095ca83aff4dcf4",
+    "url": "/static/js/2.6660cb6c.chunk.js"
+  },
+  {
+    "revision": "e15371fab3902af6e40d",
+    "url": "/static/js/main.3edcf931.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const formidable = require('formidable');
 const bodyParser=require('body-parser')
 const app = express();
+app.use(express.static(path.join(__dirname, 'frontend/build')))
 app.use(cors());
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
@@ -14,5 +15,8 @@ app.post("/login_autenticate",(req,res)=>{
  res.send(req.body)
     
 })
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+  });
 
 app.listen(8000)
