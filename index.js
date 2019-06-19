504b9fbb276ea110b323832d84abd31367bf4d63
Your Name <inboxamarbalu@gmail.com>
2019-06-19 08:02:40 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const path=require('path');
 const formidableMiddleware = require('express-formidable');
-const mongoose=require('./mongodb')
+// const mongoose=require('./mongodb')
 const app = express();
 const port=process.env.PORT || 8000;
 app.use(express.static(path.join(__dirname, 'frontend/build')))
@@ -15,8 +15,8 @@ app.get("/",(req,res)=>{
 app.post("/login_autenticate",(req,res)=>{
  res.send(req.fields)   
 })
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
-//   });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+  });
 
 app.listen(port)
--- mongodb.js
+++ mongodb.js
@@ -1,63 +1,63 @@
-const dbConfig = require('./config/database.config.js');
-const mongoose = require('mongoose');
+// const dbConfig = require('./config/database.config.js');
+// const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.url, {
-  useNewUrlParser: true
-}).then(() => {
-  console.log("Successfully connected to the database");    
-}).catch(err => {
-  console.log('Could not connect to the database. Exiting now...', err);
-  process.exit();
-});
-const loginSchema = new mongoose.Schema({
-    username:{type:String,required:true},
-    password:String,
-    date:{type:Date,defaut:Date.now},
-    isPublished:Boolean
-})
+// mongoose.Promise = global.Promise;
+// mongoose.connect(dbConfig.url, {
+//   useNewUrlParser: true
+// }).then(() => {
+//   console.log("Successfully connected to the database");    
+// }).catch(err => {
+//   console.log('Could not connect to the database. Exiting now...', err);
+//   process.exit();
+// });
+// const loginSchema = new mongoose.Schema({
+//     username:{type:String,required:true},
+//     password:String,
+//     date:{type:Date,defaut:Date.now},
+//     isPublished:Boolean
+// })
 
-const Login = mongoose.model('Login',loginSchema);
-async function createLogin(){
-const login=new Login({
-    // username:"balu",
-    password:"amarbalu1995",
-    isPublished:true
-})
-try{
-const result = await login.validate();
-console.log(result)
-}catch(ex){
-  console.log(ex.message)
-}
-}
-async function getlogin(){
-    const login=await Login.find({password: /.*balu.*/i}).select({password:1}).countDocuments();
-    console.log(login);
-}
+// const Login = mongoose.model('Login',loginSchema);
+// async function createLogin(){
+// const login=new Login({
+//     // username:"balu",
+//     password:"amarbalu1995",
+//     isPublished:true
+// })
+// try{
+// const result = await login.validate();
+// console.log(result)
+// }catch(ex){
+//   console.log(ex.message)
+// }
+// }
+// async function getlogin(){
+//     const login=await Login.find({password: /.*balu.*/i}).select({password:1}).countDocuments();
+//     console.log(login);
+// }
 
-async function updateloginrecord(){
- const search= await Login.find({username:"balu",password:"amarbalu1995"});
-updateById(search);
-}
-updateById=async(search)=>{
+// async function updateloginrecord(){
+//  const search= await Login.find({username:"balu",password:"amarbalu1995"});
+// updateById(search);
+// }
+// updateById=async(search)=>{
   
-  const login=await Login.findByIdAndUpdate("5d05c84c56ed9931706038b3",{
-    $set:{
-      isPublished:true
-    }
-  },{new:true})
-  // if(!login) return;
-  // login.set({
-  //   username:"nivi"
-  // })
-  // // login.username="balu";
-  // const res=await login.save();
-  console.log(login)
+//   const login=await Login.findByIdAndUpdate("5d05c84c56ed9931706038b3",{
+//     $set:{
+//       isPublished:true
+//     }
+//   },{new:true})
+//   // if(!login) return;
+//   // login.set({
+//   //   username:"nivi"
+//   // })
+//   // // login.username="balu";
+//   // const res=await login.save();
+//   console.log(login)
   
-}
-// getlogin();
-// updateloginrecord();
-createLogin()
+// }
+// // getlogin();
+// // updateloginrecord();
+// createLogin()
 
-module.exports=mongoose;
+// module.exports=mongoose;
