fc851d58751e3c1afda037a4803ba2a16125df96
amarbalu <37163292+amarbalu@users.noreply.github.com>
2019-06-26 11:31:23 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const {Register}=require('../collections/RegisterCollection');
 
 
 module.exports=function(passport){
-    
+     const users=[{id:"07",email:"mailnivibalu@gmail.com",password:"amar1995"},{id:"07",email:"inboxamarbalu@gmail.com",password:"amar1995"}];
     passport.serializeUser(function(user, done) {
     console.log(user)
     done(null, user);
@@ -19,30 +19,35 @@ module.exports=function(passport){
     passport.use(
         new localStrategy({usernameField:"email"},(email,password,done)=>{
            //Match User
-           
-           Register.findOne({email:email}).then(
-                user=>{
+           if (users.filter(check=>check.email=== email && check.password === password).length) {
+                console.log("1")
+                return done(null, users.filter(check=>check.email=== email && check.password === password));
+            } else {
+                return done(null, false, {"message": "User not found."});
+            }
+//            Register.findOne({email:email}).then(
+//                 user=>{
                     
-                    if(!user){
-                        return done(null,false,{message:"Email is not registered"});
-                    }
-//Match Password
+//                     if(!user){
+//                         return done(null,false,{message:"Email is not registered"});
+//                     }
+// //Match Password
 
-                    bcrypt.compare(password,user.password,(err,isMatch)=>{
+//                     bcrypt.compare(password,user.password,(err,isMatch)=>{
                       
-                        if(err)throw err;
+//                         if(err)throw err;
                         
-                        if(isMatch){
+//                         if(isMatch){
                           
-                            return done(null,user)
-                        }else{
-                          return  done(null,false,{message:"Password is incorrect"});
-                        }
-                    })
+//                             return done(null,user)
+//                         }else{
+//                           return  done(null,false,{message:"Password is incorrect"});
+//                         }
+//                     })
 
                    
-                }
-            ).catch(err=>console.log(err));  
+//                 }
+//             ).catch(err=>console.log(err));  
         })
     ) 
     
