a0da6e022f41a9dbb8fd8d61f308ba897265a1f1
Your Name <inboxamarbalu@gmail.com>
2019-06-26 22:16:31 +0530

    changes

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports={
-    url:"mongodb+srv://amarbalu:Ab@13abc@musicapp-favr0.mongodb.net/test?retryWrites=true&w=majority"
+    url:process.env.MONGOLAB_URI ||"mongodb+srv://amarbalu:Ab@13abc@musicapp-favr0.mongodb.net/test?retryWrites=true&w=majority"
 }
