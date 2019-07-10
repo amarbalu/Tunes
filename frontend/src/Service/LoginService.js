33f18ac29d47e40fc2e066248ce7ec887cf4c929
Your Name <inboxamarbalu@gmail.com>
2019-07-10 07:54:33 +0530

    changes

--- frontend/src/Service/LoginService.js
+++ frontend/src/Service/LoginService.js
@@ -17,7 +17,7 @@ function onFacebookLogin(){
         method:"GET",
         mode:"no-cors"
         // body:JSON.stringify({"access_token":"EAAWoFEvFPRIBAAvGhG6NsdZB40t1Q0ErzZCUI2ZAMNZATSHmPolUByuFtbxUEhBAwBUqiYz8GiN2ZCMs8xSwcjbEp9RpR6fw0xZBxZBtVssgpy4X4i33UT5d1IXvgvcgHgo9b5hYC394N7LDbrV9HzTRwZB7GmU6HNIz2P0KZC9ViZCwZDZD"})
-    }).then(function(res){ return res.json(); }).catch(err=>console.log(err));
+    }).then(function(res){ console.log(res) }).catch(err=>console.log(err));
 }
 
 
