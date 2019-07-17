e734b70f92b04978931faf5575250b5a0da2fa2c
amarbalu <37163292+amarbalu@users.noreply.github.com>
2019-07-17 07:42:11 +0530

    Update Login.js

--- Routes/Login.js
+++ Routes/Login.js
@@ -19,7 +19,10 @@ app.use(session({
     saveUninitialized: true }));
   app.use(passport.initialize());
   app.use(passport.session());
-
+ const myLogger = function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next()
+  }
 app.post("/onLogin",upload.none(),
  passport.authenticate('local', {
 failureRedirect: '/login/error' }),function(req, res) {
@@ -34,7 +37,7 @@ app.get("/auth/facebook/callback",passport.authenticate('facebook'
     
   res.send(req.user)
 });
-app.get('/auth/facebook',passport.authenticate('facebook'
+app.get('/auth/facebook',myLogger,passport.authenticate('facebook'
 
 ));
 
