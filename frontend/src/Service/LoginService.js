0e6310144045cc9d38cefdb025db289a40979ecf
amarbalu <37163292+amarbalu@users.noreply.github.com>
2019-07-17 07:01:43 +0530

    Update LoginService.js

--- frontend/src/Service/LoginService.js
+++ frontend/src/Service/LoginService.js
@@ -15,7 +15,7 @@ function onFacebookLogin(){
     // http://localhost:4000
     return fetch(`${process.env.REACT_APP_API_URL}/login/auth/facebook`,{
         method:"GET",
-        mode:"no-cors"
+//         mode:"no-cors"
         // body:JSON.stringify({"access_token":"EAAWoFEvFPRIBAAvGhG6NsdZB40t1Q0ErzZCUI2ZAMNZATSHmPolUByuFtbxUEhBAwBUqiYz8GiN2ZCMs8xSwcjbEp9RpR6fw0xZBxZBtVssgpy4X4i33UT5d1IXvgvcgHgo9b5hYC394N7LDbrV9HzTRwZB7GmU6HNIz2P0KZC9ViZCwZDZD"})
     }).then(function(res){ console.log(res) }).catch(err=>console.log(err));
 }
